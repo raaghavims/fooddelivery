ecdd1f2cba324f3fb69026dfb29da4a17651f7af
Raaghavi <raaghavims@gmail.com>
2024-01-18 11:49:54 +0100

    vendor profile form validations

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
@@ -16,4 +18,12 @@ class UserForm(forms.ModelForm):
         if password != confirm_password:
             raise forms.ValidationError('Passwords do not match')
 
-            
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
+
+    latitude  = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
