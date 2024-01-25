7c26e974fb1c43440a4172160b852686e0e8ee41
Raaghavi <raaghavims@gmail.com>
2024-01-25 17:50:35 +0100

    build menu and category crud

--- foodDelivery/settings.py
+++ foodDelivery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'accounts',
     'vendor',
+    "menu",
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "accounts.context_processors.get_vendor",
-                "accounts.context_processors.get_google_api"
+                "accounts.context_processors.get_google_api",
             ],
         },
     },
--- vendor/urls.py
+++ vendor/urls.py
@@ -5,5 +5,12 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
     path('profile/', views.vendor_profile, name='vendor_profile'),
-    
+    path('menu-builder/', views.menu_builder, name='menu_builder'),
+    path('menu-builder/category/<int:pk>/', views.fooditems_by_category, name='fooditems_by_category'),
+
+
+    #category CRUD
+    path('menu-builder/category/add', views.add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>', views.edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>', views.delete_category, name='delete_category'),
 ]
